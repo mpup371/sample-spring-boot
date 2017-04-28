4e2d70e86af32276d4891a098cfe52d6addc2ab5
Formation <formation@Ubuntu-16.04.2-VBox>
2017-04-28 14:31:01 +0200

    correction test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		assertEquals(True,False);
+		assertEquals(true,false);
 	}
 
 }
